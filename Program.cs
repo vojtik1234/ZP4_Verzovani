cda9140444456717b8e9133f650d98fc6bf594f0
Vojtech Ludvik <vojtik1234@gmail.com>
2023-02-20 15:54:21 +0100

    testik c.1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Vojta
+ * Date: 20.02.2023
+ * Time: 15:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace ZP4_Verzovani
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("TESTIK C.1");
+			
+			
+			Console.ReadKey(true);
+		}
+	}
+}
